3a9293f67b5b87759ef7187fe7d7c430b7241e86
Bilal Abdullah <abdullahbilal88@gmail.com>
2017-03-15 00:09:31 +0200

    Main method

--- SmsTimingTask/Program.cs
+++ SmsTimingTask/Program.cs
@@ -6,11 +6,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using SmsTimingTask.Services;
+using SmsTimingTask.Services.Impl;
 
 namespace SmsTimingTask
 {
+    /// <summary>
+    /// This class is the main entry point of the application.
+    /// </summary>
+    ///
+    /// <author>Bilal Abdullah</author>
     class Program
     {
+        /// <summary>
+        /// Represents the connection string name of a the database.
+        /// </summary>
+        private const string ConnectionStringName = "DefaultConnectionString";
+
+        /// <summary>
+        /// Represents the output path string name.
+        /// </summary>
+        private const string OutputFolderStringName = "OutputFolder";
+
+        /// <summary>
+        /// Represents the database connection string.
+        /// </summary>
+        private static readonly string connectionString = ConfigurationManager.
+               ConnectionStrings[ConnectionStringName].ConnectionString;
+
+        /// <summary>
+        /// Represents file output path the CSV files
+        /// </summary>
+        private static readonly string outputPath =
+            ConfigurationManager.AppSettings[OutputFolderStringName];
+
         /// <summary>
         /// Gets or sets the logger.
         /// </summary>
@@ -24,8 +54,40 @@ namespace SmsTimingTask
             XmlConfigurator.Configure();
             logger = LogManager.GetLogger(typeof(Program));
         }
+
+        /// <summary>
+        /// Print application help
+        /// </summary>
+        static void PrintHelp()
+        {
+            Console.WriteLine("===================================================");
+            Console.WriteLine("Enter the column name to group by!!!");
+            Console.WriteLine("===================================================");
+        }
+
+        /// <summary>
+        /// Main entry point of the program
+        /// </summary>
+        /// <param name="args">command line arguments</param>
         static void Main(string[] args)
         {
+            if (args.Length < 1)
+            {
+                PrintHelp();
+                return;
+            }
+
+            try
+            {
+                string field = args[0];
+
+                IProcess process = new DataProccess(logger, connectionString, outputPath);
+                process.Summary(field);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
